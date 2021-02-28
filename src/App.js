fa081823adf6ce7e414dd545f1669691ac4b9902
roramigator <roberto@morado.dev>
2021-02-28 11:29:12 -0500

    hero

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import Profile from "./components/Profile";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <Profile />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
---
+++ src/components/Profile.js
@@ -0,0 +1,11 @@
+import profile from "../assets/morado.svg";
+
+function Profile() {
+  return (
+    <>
+      <img src={profile} width="50%" alt="morado.dev" />
+    </>
+  );
+}
+
+export default Profile;
